f82a8f5c2982204af3a05507fec79f6fc63ca073
Rafael <rafabaccarat@gmail.com>
2021-06-29 17:24:42 -0300

    Modularizando os codigos

--- src/usuarios/estrategias-autenticacao.js
+++ src/usuarios/estrategias-autenticacao.js
@@ -4,19 +4,11 @@ const Usuario = require('./usuarios-modelo');
 const { InvalidArgumentError } = require('../erros');
 const bcrypt = require('bcrypt');
 const BearerStrategy = require('passport-http-bearer');
-const jwt = require('jsonwebtoken');
-const blocklist = require('../../redis/blocklist-access-token');
+const tokens = require('./tokens');
 
 function verificaUsuario(usuario) {
   if (!usuario) {
     throw new InvalidArgumentError('Não existe usuário com esse e-mail!');
-  }
-}
-
-async function verificaTokenNaBlocklist(token) {
-  const tokenNaBlocklist = await blocklist.contemToken(token);
-  if (tokenNaBlocklist) {
-    throw new jwt.JsonWebTokenError('Token inválido por logout! ');
   }
 }
 
@@ -49,12 +41,10 @@ passport.use(
 );
 
 passport.use(
-  new BearerStrategy(
-    async (token, done) => {
+  new BearerStrategy(async (token, done) => {
       try {
-        await verificaTokenNaBlocklist(token);
-        const payload = jwt.verify(token, process.env.CHAVE_JWT);
-        const usuario = await Usuario.buscaPorId(payload.id);
+        const id = await tokens.access.verifica(token);
+        const usuario = await Usuario.buscaPorId(id);
         done(null, usuario, { token: token });
       } catch (erro) {
         done(erro);
--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -1,26 +1,7 @@
 const Usuario = require('./usuarios-modelo');
-const { InvalidArgumentError, InternalServerError } = require('../erros');
-const jwt = require('jsonwebtoken');
+const { InvalidArgumentError } = require('../erros');
 const blocklist = require('../../redis/blocklist-access-token');
-const crypto = require('crypto');
-const moment = require('moment');
-const allowlistRefreshToken = require('../../redis/allowlist-refresh-token');
-
-function criarTokenJWT (usuario) {
-  const payload = {
-    id: usuario.id
-  };
-
-  const token = jwt.sign(payload, process.env.CHAVE_JWT, {expiresIn: '15m'}); //time da validação do token
-  return token;
-}
-
-async function criarTokenOpaco(usuario) {
-  const tokenOpaco = crypto.randomBytes(24).toString('hex');
-  const dataExpiracao = moment().add(5, 'd').unix();
-  await allowlistRefreshToken.adiciona(tokenOpaco, usuario.id, dataExpiracao);
-  return tokenOpaco;
-}
+const tokens = require('./tokens');
 
 module.exports = {
   async adiciona (req, res) {
@@ -46,8 +27,8 @@ module.exports = {
 
   async login (req, res) {
     try {
-      const accessToken = criarTokenJWT(req.user);
-      const refreshToken = await criarTokenOpaco(req.user);
+      const accessToken = tokens.access.cria(req.user.id);
+      const refreshToken = await tokens.refresh.cria(req.user.id);
       res.set('Authorization', accessToken);
       res.status(200).json({ refreshToken });
     } catch (erro) {
@@ -58,7 +39,7 @@ module.exports = {
   async logout (req, res) {
     try {
       const token = req.token;
-      await blocklist.adiciona(token);
+      await tokens.access.invalida(token);
       res.status(204).json();
     } catch (erro) {
       res.status(500).json({ erro: erro.message });
